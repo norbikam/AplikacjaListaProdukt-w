77095c49240ad05da2e22068c17b2b55170fed21
Radosław Sorbian <Radoslaw.Sorbian@drzewniak.slupsk.pl>
2021-04-16 12:31:48 +0200

    Dodaj pliki projektów.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace AplikacjaListaProduktów
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Produkt> ListaProduktow = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            PrzygotujWiazanie();
+        }
+        private void PrzygotujWiazanie()
+        {
+            ListaProduktow = new ObservableCollection<Produkt>();
+            ListaProduktow.Add(new Produkt("01-11", "ołowek", 8, "Słupsk"));
+            ListaProduktow.Add(new Produkt("PW-20", "pióro wieczne", 75, "Gdańsk"));
+            ListaProduktow.Add(new Produkt("W-20", "zeszyt 32kart", 100, "Warszawa"));
+            lstProdukty.ItemsSource = ListaProduktow;
+        }
+    }
+}
